382cfe131f42d11afb084fd0d2fc77019378adfc
mariobp <mariobarrpach@gmail.com>
2017-10-02 22:58:42 -0500

    reset password

--- Citas/urls.py
+++ Citas/urls.py
@@ -17,13 +17,18 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^dashboard/', admin.site.urls),
     url(r'^usuarios/', include('usuarios.urls')),
     url(r'^publicidad/', include('adb.urls')),
     url(r'^agenda/', include('agenda.urls')),
-    url('^', include('django.contrib.auth.urls')),
+     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
