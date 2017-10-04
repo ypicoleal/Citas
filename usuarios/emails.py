59d7e04e3e9e8fd145336ebb596c062dc6f7a264
mariobp <mariobarrpach@gmail.com>
2017-10-04 00:35:24 -0500

    citas

--- usuarios/emails.py
+++ usuarios/emails.py
@@ -13,9 +13,9 @@ def emailConfirmation(email, tipo):
     key2 = value.split(':')[2]
     activacionkey = models.ActivacionUser(email=email, key1=key1, key2=key2, tipo=tipo)
     activacionkey.save()
-    subject, from_email, to = "Confirmar cuenta", 'mariobarrios@gmail.com', [email]
+    subject, from_email, to = "Confirmar cuenta", 'info@dranilsaarias.com', [email]
     text_content = "Confirmación de cuenta"
-    html_content = "Enlace de confirmacion: http://127.0.0.1:8000/usuarios/confirmacion/?code=%s&k2=%s" % (key1, key2)
+    html_content = "Enlace de confirmacion: http://app.dranilsaarias.com/usuarios/confirmacion/?code=%s&k2=%s" % (key1, key2)
     msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(unicode(html_content, encoding='utf-8'), "text/html")
     msg.send()
