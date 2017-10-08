685c73a838f0fe40d0180ef01b3d19add008ecc1
mariobp <mariobarrpach@gmail.com>
2017-10-08 18:18:52 -0500

    update

--- agenda/forms.py
+++ agenda/forms.py
@@ -76,8 +76,9 @@ class CitaMedicaForm(forms.ModelForm):
 
 
         if hasattr(self, 'instance') and self.instance.pk:
-            calendario.queryset = models.CalendarioCita.objects.filter(inicio__year=self.instance.calendario.inicio.year, inicio__month=self.instance.calendario.inicio.month)
-            self.fields['fecha_'].initial = self.instance.calendario.inicio.strftime('%d/%m/%Y')
+            if calendario:
+                calendario.queryset = models.CalendarioCita.objects.filter(inicio__year=self.instance.calendario.inicio.year, inicio__month=self.instance.calendario.inicio.month)
+                self.fields['fecha_'].initial = self.instance.calendario.inicio.strftime('%d/%m/%Y')
         else:
             hoy = datetime.date.today()
             calendario.queryset = models.CalendarioCita.objects.filter(inicio__year=hoy.year, inicio__month=hoy.month)
