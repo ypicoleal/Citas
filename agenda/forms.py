a5e9d7a3b092a9bb4d13040d7b00dd8b732401e2
mariobp <mariobarrpach@gmail.com>
2017-10-16 19:57:25 -0500

    calendario query

--- agenda/forms.py
+++ agenda/forms.py
@@ -253,15 +253,12 @@ class ReprogramarCitaForm(forms.ModelForm):
 
     def calendario_query(self):
         if 'calendario' in self.fields:
-            raise "Entro"
             hoy = datetime.date.today()
             calendario = self.fields['calendario']
             calendario.widget.attrs['disabled'] = True
             calendario.widget.can_add_related = False
             calendario.widget.can_change_related = False
             calendario.queryset = models.CalendarioCita.objects.filter(inicio__year=hoy.year, inicio__month=hoy.month)
-        else:
-            raise "no entro, else"
     # end if
 
     def __init__(self, *args, **kwargs):
