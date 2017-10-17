6fd62064222d82e5278e63ae83f71578dc28c4d5
mariobp <mariobarrpach@gmail.com>
2017-10-16 21:53:14 -0500

    fecha calendario

--- agenda/models.py
+++ agenda/models.py
@@ -125,7 +125,9 @@ class CitaMedica(models.Model):
     # end def
 
     def save(self, *args, **kwargs):
-        self.fecha_calendario =  "%s %s" % (self.calendario.inicio.strftime('%Y-%m-%d %H:%M:%S'),  self.calendario.fin.strftime('%H:%M:%S')) 
+        if self.calendario:
+            self.fecha_calendario =  "%s %s" % (self.calendario.inicio.strftime('%Y-%m-%d %H:%M:%S'),  self.calendario.fin.strftime('%H:%M:%S'))
+        # end if 
         if self.confirmacion == 2:
             self.cancelar = True
             self.calendario = None
