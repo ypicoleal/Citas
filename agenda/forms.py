bc9dbb625a54d5dc447c045a29a213981517eff8
mariobp <mariobarrpach@gmail.com>
2017-10-16 23:34:01 -0500

    form cita

--- agenda/forms.py
+++ agenda/forms.py
@@ -258,10 +258,10 @@ class ReprogramarCitaForm(forms.ModelForm):
 
             if 'calendario' in self.fields:
                 calendario = self.fields["calendario"]
-                calendario.queryset = models.CalendarioCita.objects.filter(inicio__year=self.instance.calendario.inicio.year, inicio__month=self.instance.calendario.inicio.month, inicio__day=self.instance.calendario.inicio.day)
+                calendario.queryset = models.CalendarioCita.objects.filter(id=self.instance.calendario.id)
                 calendario.widget.can_add_related = False
                 calendario.widget.can_change_related = False
-                fecha.initial = self.instance.calendario.inicio.strftime('%d/%m/%Y')
+                #fecha.initial = self.instance.calendario.inicio.strftime('%d/%m/%Y')
                 #self.fields["motivo"].widget.attrs['disabled'] = True
             # end if
 
